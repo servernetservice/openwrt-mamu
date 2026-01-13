5fcc9da9e310cd3ee42ea9f8bd243a12a62c0e31
servernetservice <251852193+servernetservice@users.noreply.github.com>
2026-01-12 20:19:25 -0500

    Add X86_64 custom config and scripts

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-echo 'src-git small8 https://github.com/kenzok8/small-package' >> feeds.conf.default
+# 添加 Passwall 和 AdGuardHome 相关的源
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >> feeds.conf.default
+echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall;luci' >> feeds.conf.default
+# 添加其他常用插件源
+echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
--- diy-part2.sh
+++ diy-part2.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 # 设置默认主题为 bootstrap
-sed -i 's/luci-theme-bootstrap/luci-theme-bootstrap/g' feeds/luci/collections/luci/Makefile
+# 修改默认 IP (可选)
+# sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+
+# 移除 AdGuardHome 的预下载内核，减小固件体积
+rm -rf package/feeds/kenzok8/luci-app-adguardhome/root/usr/bin/AdGuardHome/AdGuardHome
+
